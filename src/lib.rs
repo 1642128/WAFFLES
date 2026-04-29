20ec4d502433ee39f575a2808e19f9a15ad8b7e0
1642128 <jmwill321jack2@gmail.com>
2026-04-29 18:17:42 -0400

    Update lib.rs

--- src/lib.rs
+++ src/lib.rs
@@ -38,19 +38,19 @@ impl Game {
     }
 
     pub fn frame(&self, _dt: f64) {
-    let canvas = self.ctx.canvas().unwrap();
-
-    self.ctx.set_fill_style(&"black".into());
-    self.ctx.fill_rect(
-        0.0,
-        0.0,
-        canvas.width() as f64,
-        canvas.height() as f64,
-    );
-
-    self.ctx.set_fill_style(&"red".into());
-    self.ctx.fill_rect(50.0, 50.0, 200.0, 200.0);
-}
+        let canvas = self.ctx.canvas().unwrap();
+    
+        self.ctx.set_fill_style_str("black");
+        self.ctx.fill_rect(
+            0.0,
+            0.0,
+            canvas.width() as f64,
+            canvas.height() as f64,
+        );
+    
+        self.ctx.set_fill_style_str("red");
+        self.ctx.fill_rect(50.0, 50.0, 200.0, 200.0);
+    }
 
     pub fn send(&self, msg: &str) {
         if let Some(ws) = &self.socket {
